c9168ce8df4c88848c2b565399ec01354657d517
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-09-02 16:30:15 +0900

    Create tbl_class_202201.sql

---
+++ School/YeonTaek_JSP/Table/insert/tbl_class_202201.sql
@@ -0,0 +1,5 @@
+insert into tbl_class_202201 values('202203', 10001, '서울본원', 100000, 100);
+insert into tbl_class_202201 values('202203', 10002, '성남분원', 100000, 100);
+insert into tbl_class_202201 values('202203', 10003, '대전분원', 200000, 200);
+insert into tbl_class_202201 values('202203', 20001, '부산분원', 150000, 300);
+insert into tbl_class_202201 values('202203', 20002, '대구분원', 200000, 400);
