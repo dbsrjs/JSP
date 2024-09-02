2c0c9f9639eefeee5ad3573193fc5ff9ece24318
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-09-02 16:29:18 +0900

    Create tbl_teacher_202201.sql

---
+++ School/YeonTaek_JSP/Table/insert/tbl_teacher_202201.sql
@@ -0,0 +1,4 @@
+insert into tbl_teacher_202201 values(100, '이초급', '초급반', 100000, '20220101');
+insert into tbl_teacher_202201 values(200, '김중급', '중급반', 200000, '20220102');
+insert into tbl_teacher_202201 values(300, '박고급', '고급반', 300000, '20220103');
+insert into tbl_teacher_202201 values(400, '정심화', '심화반', 400000, '20220104');
