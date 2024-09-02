206910c3b1329b720c348f29469ff4d81077924d
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-09-02 16:27:17 +0900

    Create tbl_teacher_202201.sql

---
+++ School/YeonTaek_JSP/Table/create/tbl_teacher_202201.sql
@@ -0,0 +1,7 @@
+create table tbl_teacher_202201 (
+	teacher_code char(3) primary key,
+	teacher_name varchar2(15),
+	class_name varchar2(20),
+	class_price number(8),
+	teach_resist_date varchar2(8)
+);
