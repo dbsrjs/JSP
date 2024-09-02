b1fb76be827f790f3a2a6d9b2c8a8092361aa128
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-09-02 16:29:45 +0900

    Create tbl_member_202201.sql

---
+++ School/YeonTaek_JSP/Table/insert/tbl_member_202201.sql
@@ -0,0 +1,5 @@
+insert into tbl_member_202201 values(10001, '홍길동', '01011112222', '서울시 강남구', '일반');
+insert into tbl_member_202201 values(10002, '장발장', '01022223333', '성남시 분당구', '일반');
+insert into tbl_member_202201 values(10003, '임꺽정', '01033334444', '대전시 유성구', '일반');
+insert into tbl_member_202201 values(20001, '성춘향', '01044445555', '부산시 서구', 'VIP');
+insert into tbl_member_202201 values(20002, '이몽룡', '01055556666', '대구시 북구', 'VIP');
