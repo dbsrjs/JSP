d4aa811ef00c3c5ba6827c569dad1eb1dd9e9ad4
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-12-19 17:58:25 +0900

    Update sql.sql

--- School/골프연습장 회원관리 프로그램/sql.sql
+++ School/골프연습장 회원관리 프로그램/sql.sql
@@ -1,51 +1,64 @@
--- tbl_teacher_202201
-create table tbl_teacher_202201 (
-    teacher_code char(3) primary key,
-    teacher_name varchar2(15),
-    class_name varchar2(20),
-    class_price number(8),
-    teach_resist_date varchar2(8)
-);
+create table student_tbl_03(
+    sno varchar2(6) not null primary key,
+    sname varchar2(10),
+    sphone varchar2(15),
+    sgender varchar2(4),
+    saddress varchar2(50)
+)
+create table exam_tbl_03(
+    sno varchar2(6) not null primary key,
+    ekor number(4),
+    emath number(4),
+    eeng number(4),
+    ehist number(4)
+)
 
-insert into tbl_teacher_202201 values('100', '이초급', '초급반', 100000, '20220101');
-insert into tbl_teacher_202201 values('200', '김중급', '중급반', 200000, '20220102');
-insert into tbl_teacher_202201 values('300', '박고급', '고급반', 300000, '20220103');
-insert into tbl_teacher_202201 values('400', '정심화', '심화반', 400000, '20220104');
+insert into student_tbl_03 values
+('10101', '김행복', '010-1111-2222', 'M', '서울 동대문구 휘경1동');
+insert into student_tbl_03 values
+('10102', '이축복', '010-1111-3333', 'F', '서울 동대문구 휘경2동');
+insert into student_tbl_03 values
+('10103', '장믿음', '010-1111-4444', 'M', '울릉군 울릉읍 독도1리');
+insert into student_tbl_03 values
+('10104', '최사랑', '010-1111-5555', 'F', '울릉군 울릉읍 독도2리');
+insert into student_tbl_03 values
+('10105', '진평화', '010-1111-6666', 'M', '제주도 제주시 외나무골');
+insert into student_tbl_03 values
+('10106', '차공단', '010-1111-7777', 'M', '제주도 제주시 감나무골');
 
-select * from tbl_teacher_202201;
+insert into exam_tbl_03 values('10101', 98, 91, 95, 90);
+insert into exam_tbl_03 values('10102', 87, 89, 92, 82);
 
--- tbl_member_202201
-create table tbl_member_202201(
-    c_no char(5) primary key,
-    c_name varchar2(15),
-    phone varchar2(11),
-    address varchar2(50),
-    grade varchar2(6)
-);
+ select sno, sname, substr(sno, 1, 1), 
+		  	  substr(sno, 2, 2), substr(sno, 4, 2), 
+   			  case sgender when 'M' then '남' 
+     		  when 'F' then '여' end sgender, 
+   		  sphone, saddress 
+		  from student_tbl_03;
+		  
+select substr(s.sno, 1, 1) 학년, substr(s.sno, 2, 2) 반, substr(s.sno, 4, 2) 번호, s.sname,
+	e.ekor 국어, e.emath 수학, e.eeng 영어, e.ehist 역사,
+	(e.ekor+e.emath+e.eeng+e.ehist) 섬, 
+	(e.ekor+e.emath+e.eeng+e.ehist/4) 평균,
+	rank() over(order by NVL(e.ekor+e.emath+e.eeng+e.ehist, 0) desc)
+from student_tbl_03 s, exam_tbl_03 e
+where s.sno = e.sno(+);
+		
+		
 
-insert into tbl_member_202201('10001', '홍길동', '010011112222', '서울시 강남구', '일반');
-insert into tbl_member_202201('10002', '장발장', '010022223333', '성남시 분당구', '일반');
-insert into tbl_member_202201('10003', '임꺽정', '010033334444', '대전시 유성구', '일반');
-insert into tbl_member_202201('10004', '성춘향', '010044445555', '부산시 서구',  'VIP');
-insert into tbl_member_202201('10005', '이몽룡', '010055556666', '대구시 북구',  'VIP');
 
-select * from tbl_member_202201;
+select sum(ekor) 국합, sum(emath) 수합,
+	sum(eeng) 영합,  sum(ehist) 역합,
+	(sum(ekor)+sum(emath)+sum(eeng)+sum(ehist)) 총합계,
+	((sum(ekor)+sum(emath)+sum(eeng)+sum(ehist)/4)) 총평균
+from exam_tbl_03;
 
--- tbl_class_202201
-create table tbl_class_202201(
-    resist_month varchar2(6),
-    c_no char(5),
-    class_area varchar2(15),
-    tuition number(8),
-    teacher_code char(3),
-    primary key(resist_month, c_no)
-);
+select avg(ekor) 국합, avg(emath) 수합,
+	avg(eeng) 영합,  avg(ehist) 역합,
+	(avg(ekor)+avg(emath)+avg(eeng)+avg(ehist)) 총합계,
+	((avg(ekor)+avg(emath)+avg(eeng)+avg(ehist)/4)) 총평균
+from exam_tbl_03;
 
-insert into tbl_class_202201 values('202203', '10001', '서울본원', 100000, '100');
-insert into tbl_class_202201 values('202203', '10002', '성남본원', 100000, '100');
-insert into tbl_class_202201 values('202203', '10003', '대전본원', 200000, '200');
-insert into tbl_class_202201 values('202203', '20001', '부산본원', 150000, '300');
-insert into tbl_class_202201 values('202203', '20002', '대구본원', 200000, '400');
 
-select * from tbl_class_202201;
 
+		
